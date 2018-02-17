24f774eb52cd10c1baa5615c99743a1a62f40e81
Maley1993 <maley1993@gmail.com>
2018-02-16 21:59:20 -0700

    added posts link to nav and button to create new post

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  
+
   resources :posts
 
   get 'about' => 'welcome#about'
+
+  get 'posts' => 'posts#index'
+
+  get 'posts_new' => 'posts#new'
 
   root 'welcome#index'
 end
