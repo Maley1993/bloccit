c24ce70c3b6b8b81df795e3555cc0498dbc9e80f
Maley1993 <maley1993@gmail.com>
2018-03-14 12:06:42 -0700

    authentication assignment complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   resources :topics do
      resources :posts, except: [:index]
    end
-   
+
   resources :users, only: [:new, :create]
+
+  post 'confirm' => 'users#confirm'
 
   get 'about' => 'welcome#about'
 
