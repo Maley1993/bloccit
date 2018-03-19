76fb8e519a70491a27cd81f1d4ae32eb29f54a80
Maley1993 <maley1993@gmail.com>
2018-03-19 15:52:35 -0700

    rails posts and users checkpoint complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+  before_action :require_sign_in, except: :show
+
   def show
     @post = Post.find(params[:id])
   end
@@ -10,13 +12,9 @@ class PostsController < ApplicationController
   end
 
   def create
-    # #9
-    @post = Post.new
-    @post.title = params[:post][:title]
-    @post.body = params[:post][:body]
     @topic = Topic.find(params[:topic_id])
-    # #35
-    @post.topic = @topic
+    @post = @topic.posts.build(post_params)
+    @post.user = current_user
 
     if @post.save
       flash[:notice] = "Post was saved."
@@ -33,8 +31,7 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    @post.title = params[:post][:title]
-    @post.body = params[:post][:body]
+    @post.assign_attributes(post_params)
 
     if @post.save
       flash[:notice] = "Post was updated."
@@ -58,4 +55,8 @@ class PostsController < ApplicationController
     end
   end
 
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
