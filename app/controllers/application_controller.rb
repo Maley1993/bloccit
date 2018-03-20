fd78ff390de945bee19aa58d236ac796756fd362
Maley1993 <maley1993@gmail.com>
2018-03-20 10:05:01 -0700

    move avatar_url method into application_helper.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
-
-  def avatar_url(user)
-    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
-  end
-
-  helper_method :avatar_url
 end
